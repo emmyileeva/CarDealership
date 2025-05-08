623522a7cdec78987fd7cee46376a892b67252e6
Emiliya Ileeva <emilyileeva@gmail.com>
2025-05-08 12:04:30 -0700

    Add Vehicle and Dealership classes with basic structure and methods

---
+++ src/main/java/com/pluralsight/Dealership.java
@@ -0,0 +1,52 @@
+package com.pluralsight;
+
+import java.util.ArrayList;
+
+public class Dealership {
+    private String name;
+    private String address;
+    private String phoneNumber;
+    private ArrayList<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phoneNumber) {
+        this.inventory = new ArrayList<>();
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByPrice(double min, double max) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByMakeModel(String make, String model) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByYear(int min, int max) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByColor(String color) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByMileage(int min, int max) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getVehiclesByType(String vehicleType) {
+        return null;
+    }
+
+    public ArrayList<Vehicle> getAllVehicles() {
+        return inventory;
+    }
+
+    public void addVehicle(Vehicle vehicle) {
+        inventory.add(vehicle);
+    }
+
+    public void removeVehicle(int vin) {
+    }
+}
